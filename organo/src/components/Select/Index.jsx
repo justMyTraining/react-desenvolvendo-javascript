69b3aec909a7f67d5f70ed512e021b85f12a1ea1
unknown <luckscarl@gmail.com>
2023-12-01 19:33:30 -0300

    fix - arrown function select

--- organo/src/components/Select/Index.jsx
+++ organo/src/components/Select/Index.jsx
@@ -4,11 +4,9 @@ function Select(props) {
   return (
     <>
       <div className="select-container w-100">
-        <label className="d-block">{props.label}</label>
+        <label className="d-block mb-2">{props.label}</label>
         <select className="select w-100">
-          {props.options.map((option) => (
-            <option key={option}>{option}</option>
-          ))}
+          {props.options.map(option => <option key={option}>{option}</option>)}
         </select>
       </div>
     </>

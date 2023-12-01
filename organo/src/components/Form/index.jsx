96fb5f03a92ba6bbcb5c8e01ee8ab1d6d9bc8bc8
unknown <luckscarl@gmail.com>
2023-12-01 17:00:11 -0300

    component - button

--- organo/src/components/Form/index.jsx
+++ organo/src/components/Form/index.jsx
@@ -1,18 +1,31 @@
+import Button from "../Button/Index";
 import Input from "../Input";
 import "./Form.css";
 
 function Form() {
+  const formSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+    console.log("Form submetido meu nobre!");
+  };
+
   return (
     <>
       <section className="container my-4 py-5">
-        <form className="form">
-          <fieldset className="mb-5">
+        <form
+          onSubmit={(e) => formSubmit(e)}
+          className="form d-flex flex-column gap-5"
+        >
+          <fieldset className="">
             <h2>Preencha os dados para criar o card do colaborador.</h2>
           </fieldset>
           <fieldset className="d-flex flex-column gap-4">
             <Input label="Nome" placeholder="Digite seu nome" />
             <Input label="Cargo" placeholder="Digite seu Cargo" />
             <Input label="Imagem" placeholder="Informe o endereço da imagem" />
+          </fieldset>
+          <fieldset>
+            <Button>Criar card</Button>
           </fieldset>
         </form>
       </section>
